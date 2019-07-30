e09708c5f0e84b9d96d40c3da5194b48cbec73f9
SadhanaNatarajan12 <37762558+SadhanaNatarajan12@users.noreply.github.com>
2019-07-30 21:45:13 +0530

    Update db_configuration.php

--- php-security/database/db_configuration.php
+++ php-security/database/db_configuration.php
@@ -9,4 +9,5 @@
     if ($con->connect_error) {
         echo $con->connect_error;
     }
+//Note: Never hardcode the password in configuration file and never use root user, this is purely for demo purposes.
 ?>
